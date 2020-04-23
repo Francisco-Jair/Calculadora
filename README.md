c3071f5843d742be9ed192bc2d3d256c2a73c5e0
Francisco Jair <fjair123@ufpi.edu.br>
2020-04-22 21:52:17 -0300

    Calculadora

---
+++ README.md
@@ -0,0 +1,14 @@
+# Calculadora
+
+Projeto de uma calculadora usado react native.
+
+## Pré-requisitos
+
+1 - [node.js](https://nodejs.org/en/)
+2 - [npm]
+3 - [expo](https://expo.io/)
+
+## Para Executar
+
+1 - npm i -> Para instalar as dependências
+2 - expo start -> Para iniciar a execução
--- src/components/Button.js
+++ src/components/Button.js
@@ -12,15 +12,39 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         borderWidth: 1,
         borderColor: '#888',
-
+    },
+    operationButton: {
+        color: '#FFFFFF',
+        backgroundColor: '#FA8231'
+    },
+    buttonDouble: {
+        width: (Dimensions.get('window').width / 4) * 2
+    },
+    buttonTriple: {
+        width: (Dimensions.get('window').width / 4) * 3
     }
 })
 
 
 export default props => {
+
+    const stylesButton = [styles.button]
+
+    if (props.double) {
+        stylesButton.push(styles.buttonDouble)
+    }
+    if (props.triple) {
+        stylesButton.push(styles.buttonTriple)
+    }
+    if (props.operation) {
+        stylesButton.push(styles.operationButton)
+    }
+    //() => props.onClick(props.label)
     return (
-        <TouchableHighlight onPress={props.onClick}>
-            <Text style={styles.button}>{props.label}</Text>
+        <TouchableHighlight onPress={() => props.onClick(props.label)}>
+            <Text style={stylesButton}>{props.label}</Text>
         </TouchableHighlight>
     )
-} 
+}
+//Como o valor esta sendo passado por paramentro aqui só precisamos passar a função no outro componente sem o valor
+        //Pois o label está definido
